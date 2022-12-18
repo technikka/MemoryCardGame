f2ece6e799c7f52054bb4aa8753b74bf4e4d17c2
Pythonikk <nikkibrusoe@gmail.com>
2022-12-18 10:35:43 -0800

    add scoreboard component

---
+++ src/components/Scoreboard.js
@@ -0,0 +1,15 @@
+import React, {useState} from "react";
+
+const Scoreboard = () => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [bestScore, setBestScore] = useState(0);
+
+  return (
+    <div className="scoreboard">
+      <div>Score: {currentScore}</div>
+      <div>Best Score: {bestScore}</div>
+    </div>
+  )
+}
+
+export default Scoreboard;
